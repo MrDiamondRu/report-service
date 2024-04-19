503338b8caa18f09881bf3c94118856818621707
rnikonov <r.nikonov-pro@yandex.ru>
2024-04-19 18:20:48 +0300

    Initial commit

---
+++ src/main/java/ru/rtk/service/reportservice/ReportServiceApplication.java
@@ -0,0 +1,18 @@
+package ru.rtk.service.reportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Основной класс для запуска сервиса.
+ *
+ * @author rnikonov
+ */
+@SpringBootApplication
+public class ReportServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportServiceApplication.class, args);
+    }
+
+}
